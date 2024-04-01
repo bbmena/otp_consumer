a329898f5f99832dc66447ff11925a19f52a6a00
Chris Mena <chris@sunnyclinical.com>
2024-04-01 11:47:11 -0600

    add wait times

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::{thread, time};
+
 use otp_service::{OtpRequest, OtpValidationRequest, password_client::PasswordClient, validator_client::ValidatorClient};
 
 pub mod otp_service {
@@ -10,13 +12,26 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Hello, world!");
     let mut client = PasswordClient::connect("http://[::1]:8080").await?;
     let mut v_client = ValidatorClient::connect("http://[::1]:8080").await?;
-    let request = tonic::Request::new(OtpRequest { timout_seconds: 30});
-    let v_request = tonic::Request::new(OtpValidationRequest { password: "12345".parse().unwrap()});
 
+    let request = tonic::Request::new(OtpRequest { username: "Joe".to_string(), timout_seconds: 30});
     let response = client.request_password(request).await?;
-    println!("Got: '{}' from service", response.into_inner().password);
+    let password = response.into_inner().password;
+    println!("Got: '{}' from service", &password);
 
+    let two_seconds = time::Duration::from_millis(2000);
+    thread::sleep(two_seconds);
+
+    let v_request = tonic::Request::new(OtpValidationRequest { username: "Joe".to_string(), password: password.clone()});
     let response = v_client.validate_password(v_request).await?;
     println!("Got: '{}' from service", response.into_inner().is_valid);
+
+    println!("Waiting 30 seconds...");
+    let thirty_seconds = time::Duration::from_millis(30000);
+    thread::sleep(thirty_seconds);
+
+    let v_request = tonic::Request::new(OtpValidationRequest { username: "Joe".to_string(), password});
+    let response = v_client.validate_password(v_request).await?;
+    println!("Got: '{}' from service", response.into_inner().is_valid);
+
     Ok(())
 }
